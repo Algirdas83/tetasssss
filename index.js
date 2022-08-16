d12591ec05b6501faba86868b1307455640d42d7
Algirdas83 <algirdasliutkus@gmail.com>
2022-08-16 23:15:36 +0300

    added midlevear for verification

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import express from 'express'
 import {engine} from 'express-handlebars'
 import mysql from 'mysql2/promise'
+import session from 'express-session'
+import auth from './midllewear/auth.js'
 
 
 const app = express()
@@ -13,6 +15,23 @@ app.use('/publick', express.static('publick'))
 app.use(express.urlencoded({
     extends: true
 }))
+
+
+// lets use session module configuration
+
+app.use(session({
+    secret:'stasiukynai',
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        
+        maxAge: 60000
+        
+    }
+
+}))
+
+
 
 
 
@@ -59,24 +78,38 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
 
-    const userData = req.body
+    try {
 
-    const user = await connection.query(`SELECT * FROM user WHERE email = '${userData.email}' AND password = '${userData.passwors}' `)
+        const userData = req.body
 
-    if(user[0].length >=1 )
-    return res.redirect('/admin')
+        const user = await connection.query(`SELECT * FROM user WHERE email = '${userData.email}' AND password = '${userData.passwors}' `)
+    
+        if(!user[0].length >=1 )
+        return res.render('login', {message:'Blogi prisijungimo duomenys'})
 
 
-    return  res.render('login')
+        req.session.loggedIn = true
+        return  res.redirect('/admin')
+        
+    } catch {
+        
+    }
+   
+
+    
 })
 
 //login handlebars end
 
 //Admin handlebars start
 
-app.get('/admin', (req, res) => {
+app.get('/admin',(auth), async (req, res) => {
 
-    res.render('admin')
+    const machines = await connection.query(`SELECT id, machine_name, about, img FROM warMachines`)
+
+
+
+    res.render('admin', {machines: machines[0]})
 })
 
 //Admin handlebars end
---
+++ midllewear/auth.js
@@ -0,0 +1,12 @@
+const auth = (req, res, next) =>{
+
+    if(!req.session.loggedIn){
+        return res.redirect('/login')
+    }
+
+    next()
+    
+}
+
+
+export default auth
